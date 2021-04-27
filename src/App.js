170db4140f12de535ff0c7c11950bb0a0e756be0
Reqven <manuu.marchand@gmail.com>
2021-04-27 18:29:25 +0200

    Material UI design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import './App.css';
 import React from 'react';
-import { HashRouter, NavLink, Route } from 'react-router-dom';
+import { HashRouter, Link, Route } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import { History } from './pages/history/History'
 import { Home } from './pages/home/Home'
 import { Stats } from './pages/stats/Stats'
 
 
-function App() {
+const App = () => {
   return (
     <HashRouter>
-      <div className="header">
-        <ul className="nav">
-          <li><NavLink exact to="/">Home</NavLink></li>
-          <li><NavLink exact to="/history">History</NavLink></li>
-          <li><NavLink exact to="/stats">Stats</NavLink></li>
-        </ul>
-      </div>
-      <div className="content">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/history" component={History} />
-        <Route exact path="/stats" component={Stats} />
-      </div>
+      <Route path="/" render={({ location }) => (
+        <Container maxWidth="sm">
+          <h1>Yam</h1>
+          <div className="nav">
+            <Tabs value={location.pathname} indicatorColor="primary" textColor="primary" variant="fullWidth">
+              <Tab component={Link} label="Home" value="/" to="/" />
+              <Tab component={Link} label="History" value="/history" to="/history" />
+              <Tab component={Link} label="Stats" value="/stats" to="/stats" />
+            </Tabs>
+          </div>
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/history" component={History} />
+            <Route exact path="/stats" component={Stats} />
+          </div>
+        </Container>
+        )}
+      />
     </HashRouter>
   );
 }
